800d0921267718d0d284346b9d5a7bae7ca874c5
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-13 11:21:34 +0900

    home route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :movies, only: [:show]
+  root to: 'lists#index'
 
   resources :lists, except: %i[edit update] do
     resources :bookmarks, only: %i[new create]
